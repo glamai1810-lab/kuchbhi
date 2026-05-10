752024c4df6808c27ecd0f4982bad3c3a71bf79e
glamai1810-lab <glamai1810@gmail.com>
2026-05-11 02:55:20 +0530

    Update analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -1,1 +1,24 @@
-
+-- Creates the table to store customer data
+CREATE TABLE customers (
+    customerID VARCHAR(50) PRIMARY KEY,
+    gender VARCHAR(10),
+    SeniorCitizen INT,
+    Partner VARCHAR(5),
+    Dependents VARCHAR(5),
+    tenure INT,
+    PhoneService VARCHAR(5),
+    MultipleLines VARCHAR(20),
+    InternetService VARCHAR(20),
+    OnlineSecurity VARCHAR(20),
+    OnlineBackup VARCHAR(20),
+    DeviceProtection VARCHAR(20),
+    TechSupport VARCHAR(20),
+    StreamingTV VARCHAR(20),
+    StreamingMovies VARCHAR(20),
+    Contract VARCHAR(20),
+    PaperlessBilling VARCHAR(5),
+    PaymentMethod VARCHAR(50),
+    MonthlyCharges DECIMAL(10, 2),
+    TotalCharges DECIMAL(10, 2),
+    Churn VARCHAR(5)
+);
